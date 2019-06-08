12ed7e4f6cdb4cd0f7886d3958ed240b0a699f42
DrGaster <flammastnguyen@gmail.com>
2019-06-07 20:41:47 -0500

    First commit

---
+++ Jarvis Mark 2/Notes.md
@@ -0,0 +1,8 @@
+store location from 
+anti-malware and anti-spyware, keeping applications upto date
+store user preferences
+order pizza
+stock market
+smoothly transitioning voice during tanslation
+translating for each region of a country with proper or given dialect of user
+open google and enter query in the search bar and retrieve relevant data
---
+++ Jarvis Mark 2/Time_of_Day_English.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+time = str(datetime.now().time())
+
+hour = int(time[0:2])
+minute = int(time[3:5])
+second = int(time[6:8])
+current_time = "%s:%s:%s" % (hour, minute, second)
+
+username = input("My name is Jarvis. What is your name? ")
+
+if hour >= 7 and hour < 11:
+    print("Good Morning,", username, "it is", current_time)
+elif hour >=11 and hour < 17:
+    print("Good Afternoon,", username, "it is", current_time)
+elif hour >=17 and hour < 22:
+    print("Good Evening,", username, "it is", current_time)
+elif hour >= 22 or hour < 7:
+    print(username, "the current time is", current_time + ". it is late you should go and get some rest.")
